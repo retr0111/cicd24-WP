820a7e08763754fa426fc4661d2760aaf858fba6
retr0111 <40544860+retr0111@users.noreply.github.com>
2024-04-22 23:47:47 -0700

    Update app.py

--- src/app.py
+++ src/app.py
@@ -2,19 +2,39 @@
     Easy Azure Streamlit Demo
     Author: Wolf Paulus (wolf@paulus.com)
 """
-from random import randint
+import random
 import streamlit as st
 from log import logger
 
+def coin_flip_with_gamble(initial_money, bet_amount, guess):
+    money = initial_money
+    result = random.choice(['Heads', 'Tails']).lower()
 
-def ui(items: [int]) -> None:
+    
+    if guess == result:
+        money += bet_amount
+        x = f"Correct! You won ${bet_amount}.\\\\\\\\ Total money: ${money}"
+        yield x
+    else:
+        money -= bet_amount
+        x = f"Wrong! You lost ${bet_amount}.\\\\\\\\ Total money: ${money}"
+        yield x
 
-    st.title("Streamlit Demo v0.4")
-    st.subheader(".. on Azure")
-    st.line_chart(items)
+def ui() -> None:
+    st.title("Coin Flip Gambling Game")
+    initial_money = 100
+    st.write("Your Cash is 100$")
+    bet_amount = st.number_input("Input the amount you would like to bet: ")
+    guess = st.text_input("Please enter Heads Or Tails").lower()
+    flip_gamble = coin_flip_with_gamble(initial_money, bet_amount, guess)
+
+    if st.button("Start Gambling"):
+        st.write(next(flip_gamble)
+
+
+
 
 
 if __name__ == "__main__":
-    data = [randint(0, 10) for _ in range(25)]
-    logger.info(f"Created a list with {len(data)} items.")
-    ui(data)
+    ui()
+
