bca5fb07d9ed941b625a69b717cb0e974e078bfa
retr0111 <40544860+retr0111@users.noreply.github.com>
2024-04-23 00:08:48 -0700

    Update app.py
    Just finishing my little app for a not so good gambling service. It doesn't keep the data for multiple use, but it is interesting.

--- src/app.py
+++ src/app.py
@@ -1,6 +1,6 @@
 """
-    Easy Azure Streamlit Demo
-    Author: Wolf Paulus (wolf@paulus.com)
+    Author: Carter Minster (Used Wolf Paulus Forked Code)
+    Code: Just a quick Coin Flip Gambling Game
 """
 import random
 import streamlit as st
@@ -14,23 +14,26 @@ def coin_flip_with_gamble(initial_money, bet_amount, guess):
     if guess == result:
         money += bet_amount
         x = f"Correct! You won ${bet_amount}.\\\\\\\\ Total money: ${money}"
-        yield x
+        return x
     else:
         money -= bet_amount
         x = f"Wrong! You lost ${bet_amount}.\\\\\\\\ Total money: ${money}"
-        yield x
+        return x
 
 def ui() -> None:
+    st.image("src/Banner.jpg")
+    st.write("Just a Bad Picture :P")
     st.title("Coin Flip Gambling Game")
     initial_money = 100
+    st.subheader("Mmmm Gambling")
     st.write("Your Cash is 100$")
     bet_amount = st.number_input("Input the amount you would like to bet: ")
     guess = st.text_input("Please enter Heads Or Tails").lower()
     flip_gamble = coin_flip_with_gamble(initial_money, bet_amount, guess)
 
     if st.button("Start Gambling"):
-        st.write(next(flip_gamble)
-
+        st.write(flip_gamble)
+    logger.info(f"{flip_gamble}")
 
 
 
